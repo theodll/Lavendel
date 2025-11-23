5ab30b37bc9b12dc75e8a23454254ce4aaceeeec
Theo Wimber <theowimber@gmail.com>
2025-11-23 21:36:51 +0100

    feat: add GameObject class

---
+++ Lavendel/src/Lavendel/Renderer/Model/GameObject.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <lvpch.h>
+
+#include "Model.h"
+
+
+namespace Lavendel
+{
+	struct TransformComponent
+	{
+		glm::vec2 translation{}; 
+
+		glm::mat2 mat2() {}
+	};
+
+	class GameObject 
+	{
+	public:
+
+		static GameObject createGameObject()
+		{
+			static uint32_t currentID = 0;
+			return GameObject{ currentID++ };
+		}
+
+		GameObject(const GameObject &) = delete;
+		GameObject &operator=(const GameObject &) = delete;
+		GameObject(GameObject &&) = default;
+		GameObject& operator=(GameObject &&) = default;
+
+
+		uint32_t getID() const { return id; }
+
+		std::shared_ptr<RenderAPI::Model> model;
+		glm::vec3 color; 
+
+	private:
+		GameObject(uint32_t objInt) : id{ objInt } {};
+		
+		uint32_t id;
+	};
+}
